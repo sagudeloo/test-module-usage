2f1f99f689e05cd4bd6fca2992e87a6d83e38944
Stiven Agudelo Osorno <stiven.agudeloo@perficient.com>
2024-07-12 16:26:48 -0500

    Opcion con release

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
 module "test-module" {
-    source = "git::https://github.com/sagudeloo/test-module.git//azure-test-module?depth=1&ref=azure-test-module-0.0.0"  
+    source = "https://github.com/sagudeloo/test-module/releases/download/azure-test-module-0.0.0/azure-test-module.zip"  
     test_variable = "hola"
 }
